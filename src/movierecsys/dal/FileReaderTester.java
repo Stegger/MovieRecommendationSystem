c8ea64d8c4331ff0c7863a84590e032872afe5df
Peter Gaarsmand Stegger Nielsen <pgn@easv.dk>
2018-11-12 12:59:25 +0100

    Removed dead code

--- src/movierecsys/dal/FileReaderTester.java
+++ src/movierecsys/dal/FileReaderTester.java
@@ -42,33 +42,5 @@ public class FileReaderTester
         System.out.println("Movie count: " + allMovs.size());
     }
 
-    private static void createUserFile() throws IOException
-    {
-        File ratings = new File("data/ratings.txt");
-        Set<Integer> uniqueIds = new HashSet<>();
-        Stream<String> lines = Files.lines(ratings.toPath());
-        lines.forEach((String t) ->
-        {
-            String[] rating = t.split(",");
-            int id = Integer.parseInt(rating[1]);
-            uniqueIds.add(id);
-        });
-        TreeSet<Integer> sorted = new TreeSet<>(uniqueIds);
-
-        File users = new File("data/users.txt");
-        try (BufferedWriter bw = Files.newBufferedWriter(users.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE))
-        {
-            File source = new File("C:\\Users\\pgn\\Desktop\\Netflix Data\\load_employees.txt");
-            List<String> emplyees = Files.readAllLines(source.toPath());
-            Iterator<Integer> it = sorted.iterator();
-            for (int i = 0; it.hasNext() && i < emplyees.size(); i++)
-            {
-                String[] cols = emplyees.get(i).split(",");
-                String firstName = cols[2].replace("\'", "");
-                String lastName = cols[3].replace("\'", "");
-                bw.write(Integer.toString(it.next()) + "," + firstName + " " + lastName);
-                bw.newLine();
-            }
-        }
-    }
+   
 }
